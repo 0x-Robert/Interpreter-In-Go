bf5429658fef0a1999e21175cb0b3c7c029b9e94
shypang <smartdragon417@gmail.com>
2023-06-05 21:13:27 +0900

    Adding :parser

---
+++ parserStudy2/src/monkey/parser/parser.go
@@ -0,0 +1,41 @@
+package parser
+
+import (
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer //현재의 렉서 인스턴스를 가리키는 포인터
+	curToken  token.Token  //현재토큰
+	peekToken token.Token  //그 다음 토큰
+}
+
+// 자기설명적이고 nextToken메서드는 curToken과 peekToken을 다음 위치로 보내는 짧은 도움 메서드다.
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.nextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
